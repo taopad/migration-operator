9e8c9e1ddc156bf394ef80a2d804d19f25f7c30a
niera26 <niera26@outlook.com>
2024-05-09 16:12:30 +0200

    boilerplate

---
+++ test/MigrationOperator.t.sol
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.25;
+
+import {Test, console} from "forge-std/Test.sol";
+
+contract MigrationOperatorTest is Test {
+    function setUp() public {}
+}
